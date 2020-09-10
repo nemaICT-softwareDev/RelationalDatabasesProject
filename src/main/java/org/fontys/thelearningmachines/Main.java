03412e52f72b56df04e28e8d2bf745853a2a1317
NemaICT <neminha13@gmail.com>
2020-09-10 14:23:49 +0200

    main updated - cleaner

--- src/main/java/org/fontys/thelearningmachines/Main.java
+++ src/main/java/org/fontys/thelearningmachines/Main.java
@@ -35,13 +35,7 @@ public class Main {
             List<MemberInterface> memberList = new MemberReader(PathNames.asMemberList()).getList().stream()
                     .map(memberDetails -> {
                         Member member = new Member();
-//                        if (memberDetails[0].equals("surname") && memberDetails[1].equals("lastname") && memberDetails[2].equals("emailaddress")
-//                                && memberDetails[3].equals("telephone") && memberDetails[4].equals("photo") && memberDetails[5].equals("nickname")
-//                                && memberDetails[6].equals("gender") && memberDetails[7].equals("dateofbirth") && memberDetails[8].equals("countryShortName")
-//                                && memberDetails[9].equals("password")) {
-//
-//                        } else {
-                         Date birthDate = new Date();
+                        Date birthDate = new Date();
                             try {
                                 /*Note the importance of the explicit Locale argument. If you omit it,
                                 then it will use the default locale which is not necessarily English
@@ -62,9 +56,9 @@ public class Main {
                             member.setDateOfBirth(birthDate);
                             member.setCountryShortName(memberDetails[8]);
                             member.setPassword(memberDetails[9]);
-                       // }
-                        return member;
-                    }).collect(Collectors.toList());
+
+                        return member;})
+                    .filter(obj -> !obj.getSurname().contains("surname")).collect(Collectors.toList());
 
             System.out.println(memberList.size());
             memberList.forEach(member -> logger.info(member.toString()));
