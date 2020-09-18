072266cc4cdd7229a8156f851814efe339570b0f
NemaICT <neminha13@gmail.com>
2020-09-18 10:33:27 +0200

    ProcessGenderData execute GenderList Procedure, updating Gender table

--- src/main/java/org/fontys/thelearningmachines/application/service/dao/ProcessGenderData.java
+++ src/main/java/org/fontys/thelearningmachines/application/service/dao/ProcessGenderData.java
@@ -1,14 +1,16 @@
 package org.fontys.thelearningmachines.application.service.dao;
 
-import org.fontys.thelearningmachines.application.entity.gender.GenderModel;
 import org.fontys.thelearningmachines.application.entity.gender.GenderInterface;
+import org.fontys.thelearningmachines.application.entity.gender.GenderModel;
+import org.fontys.thelearningmachines.application.service.reader.FileReadException;
+import org.fontys.thelearningmachines.application.service.reader.FileReaderImpl;
 import org.fontys.thelearningmachines.domain.option.ReaderOptionBuilder;
 import org.fontys.thelearningmachines.domain.option.ReaderOptionInterface;
-import org.fontys.thelearningmachines.application.service.reader.FileReaderImpl;
-import org.fontys.thelearningmachines.application.service.reader.FileReadException;
 import org.fontys.thelearningmachines.domain.value.PathNames;
+import org.fontys.thelearningmachines.infrastructure.database.DatabaseConnection;
 import org.slf4j.Logger;
 
+import java.sql.*;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -24,6 +26,20 @@ public final class ProcessGenderData {
                 .map(gender -> new GenderModel(gender[0]))
                 .collect(Collectors.toList());
 
-        genderList.forEach(model -> logger.info(model.toString()));
+        String connectionUrl = new DatabaseConnection().getConnectionUrl();
+
+        try (Connection connection = DriverManager.getConnection(connectionUrl)) {
+            for (GenderInterface gender : genderList) {
+                PreparedStatement statement = connection.prepareStatement("EXECUTE dbo.GenderList ?;", Statement.RETURN_GENERATED_KEYS);
+
+                statement.setString(1, gender.getGender());
+
+                statement.execute();
+
+                logger.info(gender.toString());
+            }
+        } catch (SQLException e) {
+            logger.error("{}", e.getMessage());
+        }
     }
 }
